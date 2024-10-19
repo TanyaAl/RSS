c0e7d39c515ef59505a85e72c6a3fcf165db654c
tatiana <alks.tatiana@gmail.com>
2024-10-19 19:48:24 +0700

    step2stop

---
+++ src/js/view.js
@@ -0,0 +1,28 @@
+// import $ from 'jquery';
+
+// const initView = (watchedState, path, value) => {
+//     const ul = $('.list-group');
+//     switch (path) {
+//       case 'rssForm.currentFeed':
+//         if (Object.keys(watchedState.errors).length !== 0) {
+//           $('.form-control').addClass('is-invalid');
+//           $('.invalid-feedback').remove();
+//           const feedback = document.querySelector('.feedback');
+//           feedback.textContent = watchedState.errors.input;
+//         } else {
+//           $('.form-control').removeClass('is-invalid');
+//           feedback.textContent = '';
+//         }
+//         break;
+//       case 'rssForm.feeds':
+//         feedback.textContent = '';
+//         if (watchedState.rssForm.stateForm === 'valid') {
+
+//         }
+//         break;
+//       default:
+//         break;
+//     }
+// };
+
+// export default initView;
